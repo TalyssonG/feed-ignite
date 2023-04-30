d49f6e82a15364a3dc77b878f4a1349057b918c3
TalyssonG <lopestalysson99@gmail.com>
2023-04-29 21:38:36 -0300

    Post

---
+++ src/components/Post.jsx
@@ -0,0 +1,31 @@
+import styles from './Post.module.css';
+
+export function Post(){
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/TalyssonG.png" />
+                    <div className={styles.authorInfo}>
+                        <strong>Talysson Gadêlha</strong>
+                        <span>Front-End developer</span>
+                    </div>
+                </div>
+
+                <time title="20 de Abril ás 21:30h" dateTime="2023-04-29 21:26:30">Publicado há 1h</time>
+            </header>
+
+        <div className={styles.content}>
+                    <p> Fala galeraa 👋 </p>
+
+                    <p> Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+                    <p> 👉 <a href=""> jane.design/doctorcare</a></p>
+
+                    <p> <a href=""> #novoprojeto #nlw #rocketseat</a></p>
+            
+        </div>
+
+        </article>
+    )
+}
